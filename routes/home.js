63fe07fa84f2023ab25fb3f857f39e2cce561353
dennisappiahALX <appiahdennis1998@gmail.com>
2022-10-23 22:29:31 +0000

    del

--- routes/home.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req,res) => {
-    res.render('app', { title: 'Vidly', message: 'Hello!'});
-});
-
-
-module.exports = router;
