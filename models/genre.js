d47dc349a0a4441ee6e8ddc27dc4af54b68d2447
dennisappiahALX <appiahdennis1998@gmail.com>
2022-10-24 01:14:07 +0000

    models

--- models/genre.js
+++ models/genre.js
@@ -2,14 +2,15 @@ const Joi = require('joi');
 const mongoose = require('mongoose');
 
 
-
-const Genre = mongoose.model('Genre', new mongoose.Schema({
+const genreSchema = new mongoose.Schema({
     name : { 
         type : String, 
         required : true, 
         minlength : 5 , 
         maxlength : 50}
-}));
+})
+
+const Genre = mongoose.model('Genre',genreSchema);
 
 function validateGenre(genre){
     const schema = Joi.object({ 
@@ -19,4 +20,5 @@ function validateGenre(genre){
 }
 
 exports.Genre= Genre;
+exports.genreSchema = genreSchema;
 exports.validate = validateGenre;
---
+++ models/rental.js
@@ -0,0 +1,75 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+
+const Rental = mongoose.model('Rental', new mongoose.Schema({
+//customising parameter in sub-documents- we need to do this since we might not need all the parameters
+    customer : { 
+       type : new mongoose.Schema({
+        name : { 
+            type : String, 
+            required : true, 
+            minlength : 5 ,
+            maxlength : 50},
+        isGold : {
+            type : Boolean,
+            default : false
+        },
+        phone : { 
+            type : String, 
+            required : true, 
+            minlength : 5 , 
+            maxlength : 50}
+       }),
+       required : true
+        },
+
+    movie : {
+      type : new mongoose.Schema({
+        title : { 
+            type : String, 
+            required : true, 
+             minlength : 5 ,
+             maxlength : 255,
+             trim : true
+            },
+        dailyRentalRate : {
+            type : Number,
+            required : true,
+            min : 0,
+            max : 255
+        }    
+        }),
+      required : true
+    },
+
+    dateOut : {
+        type : Date,
+        required : true,
+        default : Date.now()
+    },
+
+    dateReturned : {
+        type : Date   
+    },
+
+    rentalFee : {
+        type : Number,
+        min : 0   
+    }
+
+}));
+
+//validate the request body
+function validateRental(rental){
+    const schema = Joi.object({ 
+        customerId : Joi.objectId().required(),
+        movieId : Joi.objectId().required()
+    });
+    
+    return schema.validate(rental);
+}
+
+
+exports.Rental = Rental;
+exports.validate = validateRental;
